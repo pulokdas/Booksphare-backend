6fa1c863fe80df920de3216ba693b94026b89625
pulokdas <pulokdas005m>
2023-12-03 22:02:10 +0600

    Vercel issue

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ const client = new MongoClient(uri, {
   }
 });
 const BooksphareDB = client.db("BooksphareDB");
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// git
 
 
 app.get('/', (req , res)=>{
